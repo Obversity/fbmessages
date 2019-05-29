8b9425fde04572b8cb273cf05560b5673e7bcc20
bill <bill.j.pearce@gmail.com>
2019-05-30 01:03:44 +0930

    fixed relationships and destroy dependencies

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,4 @@
 class Conversation < ApplicationRecord
-  has_many :participants
-  has_many :messages, dependent: :destroy
+  has_many :participants, dependent: :destroy
+  has_many :messages
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,7 +1,7 @@
 class Participant < ApplicationRecord
   belongs_to :person
   belongs_to :conversation
-  has_many :messages
+  has_many :messages, dependent: :delete_all
 
   delegate :name, to: :person
 end

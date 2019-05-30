d30218d61b18782b6255525947b7dc49a61a53a6
bill <bill.j.pearce@gmail.com>
2019-05-30 16:16:45 +0930

    added some stats for videos sent, added pie charts for total stats

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,7 +9,14 @@ class Message < ApplicationRecord
     where("content ilike ?", "%#{keywords}%")
   }
 
+  scope :voice_messages, ->{ where('content like ?', "%sent a voice message.") }
+  scope :not_voice_messages, ->{ where.not('content like ?', "%sent a voice message.") }
+
+  scope :videos, ->{ where('content like ?', '%sent a video.') }
+  scope :not_videos, ->{ where.not('content like ?', '%sent a video.') }
+
   scope :missed_calls, ->{ where("content like ?", "%missed a call from%") }
   scope :missed_calls, ->{ where("content like ?", "%missed a video chat with%") }
   scope :video_chats, ->{ where('content = ?', "The video chat ended.") }
+
 end
